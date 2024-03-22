1a1c593ef8f8b867066523d2e554f2e4f5b021d1
anna.deineko <anna.deineko@student.kdg.be>
2024-03-22 17:46:17 +0100

    temporarily commented out security

--- src/main/java/be/kdg/team9/integration4/config/SecurityConfig.java
+++ src/main/java/be/kdg/team9/integration4/config/SecurityConfig.java
@@ -1,125 +1,61 @@
-//package be.kdg.team9.integration4.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-//import static org.springframework.security.web.util.matcher.RegexRequestMatcher.regexMatcher;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        // @formatter:off
-//        http.authorizeHttpRequests(
-//            auths -> auths
-//                    .requestMatchers(regexMatcher("^/(surveys|surveys\\/?./questions)"))
-//                        .permitAll()
-//                    .requestMatchers(
-//                            antMatcher(HttpMethod.GET, "/js/**"),
-//                            antMatcher(HttpMethod.GET, "/css/**"),
-//                            antMatcher(HttpMethod.GET, "/webjars/**"),
-//                            regexMatcher(HttpMethod.GET, "\\.ico$"))
-//                        .permitAll()
-//                    .requestMatchers(
-//                            antMatcher(HttpMethod.GET, "/api/**"))
-//                        .permitAll()
-//                    .requestMatchers(antMatcher(HttpMethod.GET, "/"))
-//                        .permitAll()
-//                    .anyRequest()
-//                        .authenticated()
-//            )
-//                .formLogin(formLogin ->
-//                        formLogin
-//                                .loginPage("/login")
-//                                .permitAll())
-//                .exceptionHandling(exceptionHandling ->
-//                        exceptionHandling.authenticationEntryPoint(
-//                                (request, response, exception) -> {
-//                                    if (request.getRequestURI().startsWith("/api")) {
-//                                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-//                                    } else {
-//                                        response.sendRedirect(request.getContextPath() + "/login");
-//                                    }
-//                                })
-//                );
-//        // @formatter:on
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
-=======
-package be.kdg.team9.integration4.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-import static org.springframework.security.web.util.matcher.RegexRequestMatcher.regexMatcher;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.authorizeHttpRequests(
-            auths -> auths
-                    .requestMatchers(regexMatcher("^/(surveys|surveys\\/?./questions)"))
-                        .permitAll()
-                    .requestMatchers(
-                            antMatcher(HttpMethod.GET, "/js/**"),
-                            antMatcher(HttpMethod.GET, "/css/**"),
-                            antMatcher(HttpMethod.GET, "/webjars/**"),
-                            regexMatcher(HttpMethod.GET, "\\.ico$"))
-                        .permitAll()
-                    .requestMatchers(
-                            antMatcher(HttpMethod.GET, "/api/**"))
-                        .permitAll()
-                    .requestMatchers(antMatcher(HttpMethod.GET, "/"))
-                        .permitAll()
-                    .anyRequest()
-                        .authenticated()
-            )
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/surveys", true)
-                                .permitAll())
-                .exceptionHandling(exceptionHandling ->
-                        exceptionHandling.authenticationEntryPoint(
-                                (request, response, exception) -> {
-                                    if (request.getRequestURI().startsWith("/api")) {
-                                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                                    } else {
-                                        response.sendRedirect(request.getContextPath() + "/login");
-                                    }
-                                })
-                );
-        // @formatter:on
-        return http.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
->>>>>>> 242dac46eb9f591f776cad0079bb9e6088c0ecff
+////package be.kdg.team9.integration4.config;
+////
+////import org.springframework.context.annotation.Bean;
+////import org.springframework.context.annotation.Configuration;
+////import org.springframework.http.HttpMethod;
+////import org.springframework.http.HttpStatus;
+////import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+////import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+////import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+////import org.springframework.security.web.SecurityFilterChain;
+////
+////import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+////import static org.springframework.security.web.util.matcher.RegexRequestMatcher.regexMatcher;
+////
+////@Configuration
+////@EnableWebSecurity
+////public class SecurityConfig {
+////    @Bean
+////    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+////        // @formatter:off
+////        http.authorizeHttpRequests(
+////            auths -> auths
+////                    .requestMatchers(regexMatcher("^/(surveys|surveys\\/?./questions)"))
+////                        .permitAll()
+////                    .requestMatchers(
+////                            antMatcher(HttpMethod.GET, "/js/**"),
+////                            antMatcher(HttpMethod.GET, "/css/**"),
+////                            antMatcher(HttpMethod.GET, "/webjars/**"),
+////                            regexMatcher(HttpMethod.GET, "\\.ico$"))
+////                        .permitAll()
+////                    .requestMatchers(
+////                            antMatcher(HttpMethod.GET, "/api/**"))
+////                        .permitAll()
+////                    .requestMatchers(antMatcher(HttpMethod.GET, "/"))
+////                        .permitAll()
+////                    .anyRequest()
+////                        .authenticated()
+////            )
+////                .formLogin(formLogin ->
+////                        formLogin
+////                                .loginPage("/login")
+////                                .permitAll())
+////                .exceptionHandling(exceptionHandling ->
+////                        exceptionHandling.authenticationEntryPoint(
+////                                (request, response, exception) -> {
+////                                    if (request.getRequestURI().startsWith("/api")) {
+////                                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+////                                    } else {
+////                                        response.sendRedirect(request.getContextPath() + "/login");
+////                                    }
+////                                })
+////                );
+////        // @formatter:on
+////        return http.build();
+////    }
+////
+////    @Bean
+////    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+////}
