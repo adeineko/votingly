8398056355746ddd64b487c3fb4080429b45c324
Sofiia Hmyria <sofiia.hmyria@student.kdg.be>
2024-04-26 11:58:42 +0200

    Changes in security, thank you page design

--- src/main/java/be/kdg/team9/integration4/controller/api/AccountsController.java
+++ src/main/java/be/kdg/team9/integration4/controller/api/AccountsController.java
@@ -6,17 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
-
-/*
-* Accounts page is working but not using button MyAccount
-* To find it add id parameter to the url eg. http/localhost/8080/account/1
-* */
 
 @Controller
 @RequestMapping("/api/account")
@@ -30,7 +21,7 @@ public class AccountsController {
         this.modelMapper = modelMapper;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<UserDto> getOneUser(@PathVariable("id") long userId) {
         var oneUser = userService.getUserById(userId);
         if (oneUser == null) {
