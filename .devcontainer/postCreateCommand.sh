78d8a2388986020229825b5881f2695de3e6c3e5
Ross Rucka <rostislav.rucka@student.kdg.be>
2024-04-28 15:18:26 +0000

    CICD testing build and deploy

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,6 +8,8 @@ cd /workspace
 # sudo chown -R vscode:vscode .
 # sudo chown -R vscode:vscode ~/.ssh
 # chmod  0700 ~/.ssh; chmod 600 ~/.ssh/*
-sudo su -c "alias ssha='eval $(ssh-agent) && ssh-add /home/vscode/.ssh/id_rsa'" vscode
+# sudo 
+su -c "alias ssha='eval $(ssh-agent) && ssh-add /home/vscode/.ssh/id_rsa'" vscode
 # source ~/.bashrc
-sudo su -c "ssha" vscode
+# sudo 
+su -c "ssha" vscode
