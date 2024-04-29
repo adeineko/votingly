bbe500ec1b4f2521c0a170ca7fdb10afad9d074a
Sofiia Hmyria <sofiia.hmyria@student.kdg.be>
2024-04-29 12:21:07 +0200

    Login design and Signup, csrf changes

--- src/main/java/be/kdg/team9/integration4/repositories/UserRepository.java
+++ src/main/java/be/kdg/team9/integration4/repositories/UserRepository.java
@@ -2,8 +2,13 @@ package be.kdg.team9.integration4.repositories;
 
 import be.kdg.team9.integration4.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(long id);
     User findByFirstName(String firstName);
+
+//    @Query("SELECT u FROM User u WHERE u.email = ?1")
+//    public User findByEmail(String email);
+
 }
