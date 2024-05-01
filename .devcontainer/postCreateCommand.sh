d33a76920c74b2ea223353ae9f66a2982f096cbd
rosstarz <rqr707@gmail.com>
2024-05-01 21:19:43 +0000

    CICD multi testing

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,10 +8,6 @@ cd /workspace
 # sudo chown -R vscode:vscode .
 # sudo chown -R vscode:vscode ~/.ssh
 # chmod  0700 ~/.ssh; chmod 600 ~/.ssh/*
-# sudo su -c "
-echo "alias ssha='eval \$(ssh-agent) && ssh-add /home/vscode/.ssh/id_rsa'" >> ~/.bashrc
-eval $(ssh-agent) && ssh-add /home/vscode/.ssh/id_rsa
-# ssh-agent
-# source ~/.bashrc && ssha
+
 mkdir -p ~/.gradle
 echo springProfilesActiveTests=testcontainer >>  ~/.gradle/gradle.properties
