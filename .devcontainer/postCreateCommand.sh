f91deb2be3e366e4fca244e24ca06190fab61907
rosstarz <rqr707@gmail.com>
2024-05-01 20:53:56 +0000

    improving devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,7 +10,8 @@ cd /workspace
 # chmod  0700 ~/.ssh; chmod 600 ~/.ssh/*
 # sudo su -c "
 echo "alias ssha='eval \$(ssh-agent) && ssh-add /home/vscode/.ssh/id_rsa'" >> ~/.bashrc
-source ~/.bashrc
-ssha
+eval $(ssh-agent) && ssh-add /home/vscode/.ssh/id_rsa
+# ssh-agent
+# source ~/.bashrc && ssha
 mkdir -p ~/.gradle
 echo springProfilesActiveTests=testcontainer >>  ~/.gradle/gradle.properties
