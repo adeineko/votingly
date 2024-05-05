b30e84c321183a19e8ce7e72c757b22922368190
rosstarz <rqr707@gmail.com>
2024-05-05 14:38:15 +0000

    CICD finishing deployment

--- deploy/vm_res/deploy.sh
+++ deploy/vm_res/deploy.sh
@@ -6,6 +6,6 @@
 # sudo su -c "docker login registry.gitlab.com -u int4t9-deploy-token --password-stdin <<< $(docker run -ti --name gcloud-config gcr.io/google.com/cloudsdktool/google-cloud-cli gcloud secrets versions access latest --secret="GITLAB_DEPLOY_TOKEN")" admin
 
 # Rerun docker compose to pull the latest image
-sudo su -c "cd ~ &&
-docker compose down &&
-docker compose up -d" admin
+cd ~
+docker compose down
+docker compose up -d
