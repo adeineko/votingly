2191c39ab09299cf0a8b332c5cf7c4df2ba361e8
anna.deineko <anna.deineko@student.kdg.be>
2024-05-06 10:41:51 +0200

    conflict

--- src/test/java/be/kdg/team9/integration4/controller/api/AnswersControllerTest.java
+++ src/test/java/be/kdg/team9/integration4/controller/api/AnswersControllerTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 class AnswersControllerTest {
     @Autowired
     private MockMvc mockMvc;
